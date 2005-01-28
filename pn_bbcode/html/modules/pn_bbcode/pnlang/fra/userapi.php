b532755136651efc1b6513be94c4988cf3e0d114
landseer <landseer@7f1c3105-0615-0410-b688-9463bc95ded3>
2005-01-28 12:33:15 +0000

    moved all bbcode related stuff to the pn_bbcode hook, necessary javascript now in external file, will be loaded only when needed

---
+++ pn_bbcode/html/modules/pn_bbcode/pnlang/fra/userapi.php
@@ -0,0 +1,93 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2001 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Original Author of file: Hinrich Donner
+// changed to pn_bbcode: larsneo
+// ----------------------------------------------------------------------
+
+define('_PNBBCODE_BOLD_HINT', 'texte gras');
+define('_PNBBCODE_BOLD', 'b');
+define('_PNBBCODE_CODE_HINT', 'insérer du code');
+define('_PNBBCODE_CODE', 'Code');
+define('_PNBBCODE_COLOR_BLACK', 'Noir');
+define('_PNBBCODE_COLOR_BLUE', 'Bleu');
+define('_PNBBCODE_COLOR_BROWN', 'Marron');
+define('_PNBBCODE_COLOR_CYAN', 'Cyan');
+define('_PNBBCODE_COLOR_DARKBLUE', 'Bleu foncé');
+define('_PNBBCODE_COLOR_DARKRED', 'Rouge foncé');
+define('_PNBBCODE_COLOR_DEFAULT', 'Standard');
+define('_PNBBCODE_COLOR_DEFINE', 'couleur personnalisée');
+define('_PNBBCODE_COLOR_GREEN', 'Vert');
+define('_PNBBCODE_COLOR_HINT', 'sélectionner la couleur du texte');
+define('_PNBBCODE_COLOR_INDIGO', 'Indigo');
+define('_PNBBCODE_COLOR_OLIVE', 'Olive');
+define('_PNBBCODE_COLOR_ORANGE', 'Orange');
+define('_PNBBCODE_COLOR_RED', 'Rouge');
+define('_PNBBCODE_COLOR_TEXTCOLORCODE', 'entrez le code de la couleur');
+define('_PNBBCODE_COLOR_VIOLET', 'Violet');
+define('_PNBBCODE_COLOR_WHITE', 'Blanc');
+define('_PNBBCODE_COLOR_YELLOW', 'Jaune');
+define('_PNBBCODE_ENTER_EMAIL_ADDRESS','Entrez l\'adresse email que vous désirez ajouter :');
+define('_PNBBCODE_ENTER_LIST_ITEM','Entrez la nouvelle liste des items. Notez que chaque liste doit commencer par une ouverture de liste et se terminer par une fermeture de liste :');
+define('_PNBBCODE_ENTER_PAGE_TITLE','Titre de la page :');
+define('_PNBBCODE_ENTER_SITE_TITLE','Entrez le titre du site web :');
+define('_PNBBCODE_ENTER_TEXT_BOLD','Entrer le texte que vous voulez mettre en gras :');
+define('_PNBBCODE_ENTER_TEXT_ITALIC','Entrer le texte que vous voulez mettre en italique :');
+define('_PNBBCODE_ENTER_TEXT_UNDERLINE','entrez le texte à souligner');
+define('_PNBBCODE_ENTER_URL','Entrez l\'URL du lien à ajouter :');
+define('_PNBBCODE_ENTER_WEBIMAGE_URL','Entrez l\'URL de l\'image que vous voulre afficher :');
+define('_PNBBCODE_FONTCOLOR', 'Couleur de la police');
+define('_PNBBCODE_FONTSIZE', 'Taille de la police');
+define('_PNBBCODE_IMAGE_HINT', 'insérer une image');
+define('_PNBBCODE_IMAGE', 'Image');
+define('_PNBBCODE_ITALIC_HINT', 'texte italique');
+define('_PNBBCODE_ITALIC', 'i');
+define('_PNBBCODE_LISTCLOSE_HINT', 'fin de liste');
+define('_PNBBCODE_LISTCLOSE', '/ul');
+define('_PNBBCODE_LISTITEM_HINT', 'élément de liste');
+define('_PNBBCODE_LISTITEM', 'li');
+define('_PNBBCODE_LISTOPEN_HINT', 'debut de liste');
+define('_PNBBCODE_LISTOPEN', 'ul');
+define('_PNBBCODE_MAIL_HINT', 'insérer une adresse email');
+define('_PNBBCODE_MAIL', 'Adresse email');
+define('_PNBBCODE_QUOTE_HINT', 'insérer une citation');
+define('_PNBBCODE_QUOTE', 'Quote');
+define('_PNBBCODE_SIZE_DEFINE', 'taille personnalisée');
+define('_PNBBCODE_SIZE_HINT', 'Sélectionner la taille de la police');
+define('_PNBBCODE_SIZE_HUGE', 'Géant');
+define('_PNBBCODE_SIZE_LARGE', 'Grand');
+define('_PNBBCODE_SIZE_NORMAL', 'Normal');
+define('_PNBBCODE_SIZE_SMALL', 'Petit');
+define('_PNBBCODE_SIZE_TEXTSIZE', 'entrer la taille du texte');
+define('_PNBBCODE_SIZE_TINY', 'Minuscule');
+define('_PNBBCODE_UNDERLINE_HINT', 'texte souligné');
+define('_PNBBCODE_UNDERLINE', 'u');
+define('_PNBBCODE_URL_HINT', 'insérer un lien');
+define('_PNBBCODE_URL', 'URL');
+
+define('_PNBBCODE_ARGSERROR',                 '[pn_bbcode] Internal error! Arguments missing!');
+define('_PNBBCODE_CODE', 'Code');
+define('_PNBBCODE_QUOTE', 'Quote');
+?>
