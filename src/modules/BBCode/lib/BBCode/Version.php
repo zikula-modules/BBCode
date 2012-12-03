39785baf24c50518e9ab713fcab8cd27447c279d
Craig Heydenburg <craigh@mac.com>
2012-12-02 19:42:15 -0500

    update version description

--- src/modules/BBCode/lib/BBCode/Version.php
+++ src/modules/BBCode/lib/BBCode/Version.php
@@ -15,8 +15,7 @@ class BBCode_Version extends Zikula_AbstractVersion
         $meta = array();
         $meta['oldnames'] = array('pn_bbcode', 'bbcode');
         $meta['version'] = '3.0.0';
-        $meta['id'] = '164';
-        $meta['description'] = __('BBCode is a transform hook that converts usual BBCode tags into html.');
+        $meta['description'] = __('BBCode is a filter hook that converts usual BBCode tags into html.');
         $meta['displayname'] = __('BBCode Hook');
         //! module url (lower case without spaces and different to displayname)
         $meta['url'] = 'bbcode';
