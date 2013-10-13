17ddfe17dcc42b6fb3ac25bf762ae86e9572ec5f
Craig Heydenburg <craigh@mac.com>
2013-10-13 18:44:26 -0400

    set min core version to 1.3.6

--- lib/BBCode/Version.php
+++ lib/BBCode/Version.php
@@ -15,6 +15,7 @@ class BBCode_Version extends Zikula_AbstractVersion
         $meta = array();
         $meta['oldnames'] = array('pn_bbcode', 'bbcode');
         $meta['version'] = '3.0.0';
+        $meta['core_min'] = '1.3.6';
         $meta['description'] = __('BBCode is a filter hook that converts usual BBCode tags into html.');
         $meta['displayname'] = __('BBCode Hook');
         //! module url (lower case without spaces and different to displayname)
